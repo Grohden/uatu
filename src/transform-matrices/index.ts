87a5d6741a9db262ad405eb2b1bb5f51086e2275
gabriel.rohden <gabriel_rohden@outlook.com>
2022-09-14 15:26:42 -0300

    feat: general rotate matrix

--- src/transform-matrices/index.ts
+++ src/transform-matrices/index.ts
@@ -1,4 +1,5 @@
 export { projMatrix } from "./projection";
+export { rotateMatrix } from "./rotate";
 export { rotateXMatrix } from "./rotateX";
 export { rotateYMatrix } from "./rotateY";
 export { rotateZMatrix } from "./rotateZ";
---
+++ src/transform-matrices/rotate.ts
@@ -0,0 +1,18 @@
+import { Mat4x4 } from "../structs";
+
+export const rotateMatrix = (xTheta: number, yTheta: number, zTheta: number): Mat4x4 => [
+  [
+    Math.cos(zTheta) * Math.cos(yTheta),
+    Math.cos(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) - Math.sin(zTheta) * Math.cos(xTheta),
+    Math.cos(zTheta) * Math.sin(yTheta) * Math.cos(xTheta) + Math.sin(zTheta) * Math.sin(xTheta),
+    0,
+  ],
+  [
+    Math.sin(zTheta) * Math.cos(yTheta),
+    Math.sin(zTheta) * Math.sin(yTheta) * Math.sin(xTheta) + Math.cos(zTheta) * Math.cos(xTheta),
+    Math.sin(zTheta) * Math.sin(yTheta) * Math.cos(xTheta) - Math.cos(zTheta) * Math.sin(xTheta),
+    0,
+  ],
+  [-Math.sin(yTheta), Math.cos(yTheta) * Math.sin(xTheta), Math.cos(yTheta) * Math.cos(xTheta), 0],
+  [0, 0, 0, 1],
+];
