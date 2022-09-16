d9026328825af9090ab30b6ab4ec655f7633af67
gabriel.rohden <gabriel_rohden@outlook.com>
2022-09-16 10:42:00 -0300

    perf: allow makeTriangleDrawer to optimize strokeStyle set calls

--- src/canvasDrawers.ts
+++ src/canvasDrawers.ts
@@ -1,5 +1,17 @@
 import { CanvasWrapper } from "./canvasWrapper";
 
+const memoizedSet = <T>(
+  setter: (newValue: T) => void,
+) => {
+  let last: T | null = null;
+  return (value: T) => {
+    if (value !== last) {
+      last = value;
+      setter(last);
+    }
+  };
+};
+
 export const canvasFill = (wrapper: ReturnType<typeof CanvasWrapper>) =>
 (
   x: number,
@@ -35,27 +47,39 @@ export const makeLineDrawer = (wrapper: ReturnType<typeof CanvasWrapper>) =>
   context.closePath();
 };
 
-export const makeTriangleDrawer = (wrapper: ReturnType<typeof CanvasWrapper>) =>
-(
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number,
-  x3: number,
-  y3: number,
-  color: string,
+export const makeTriangleDrawer = (
+  wrapper: ReturnType<typeof CanvasWrapper>,
+  // Might cause weird behaviours
+  optimized = true,
 ) => {
-  const { context } = wrapper;
+  const setStrokeColor = memoizedSet<string>((newColor) => {
+    wrapper.context.strokeStyle = newColor;
+  });
 
-  context.beginPath();
-  // FIXME: perf for this call is really poor
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.lineTo(x3, y3);
-  context.lineTo(x1, y1);
-  context.strokeStyle = color;
-  context.stroke();
-  context.closePath();
+  return (
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    x3: number,
+    y3: number,
+    color: string,
+  ) => {
+    const { context } = wrapper;
+
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.lineTo(x3, y3);
+    context.lineTo(x1, y1);
+    if (optimized) {
+      setStrokeColor(color);
+    } else {
+      context.strokeStyle = color;
+    }
+    context.stroke();
+    context.closePath();
+  };
 };
 
 export const makeTriangleFiller = (wrapper: ReturnType<typeof CanvasWrapper>) =>
