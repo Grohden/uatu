c6165028ca0ede1758726fb0bab9d2977e71cdfe
gabriel.rohden <gabriel_rohden@outlook.com>
2022-09-20 23:25:22 -0300

    chore: change compiler to esbuild

--- dev-server.js
+++ dev-server.js
@@ -3,12 +3,12 @@ const static = require("serve-static");
 
 const server = connect();
 
-server.use(static(__dirname + "/dist"));
+server.use(static(__dirname + "/build"));
 
 server.listen(8080);
 
 const livereload = require("livereload");
 const lrserver = livereload.createServer();
-lrserver.watch(__dirname + "/dist");
+lrserver.watch(__dirname + "/build");
 
 console.log("http://localhost:8080/");
